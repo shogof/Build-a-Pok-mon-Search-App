2e692d1412e0d6622cc190ce3e107b444af08bbf
Shegofa <shogofadeveloper12@gmail.com>
2024-07-11 13:57:08 +0400

    style.css linters error solved

--- search.js
+++ search.js
@@ -2,11 +2,25 @@ const inputElement = document.querySelector("#search-input");
 const search_icon = document.querySelector("#search-close-icon");
 const sort_wrapper = document.querySelector(".sort-wrapper");
 
+function search() {
+    document.querySelector("#search-input").value = "";
+    document
+      .querySelector("#search-close-icon")
+      .classList.remove("search-close-icon-visible");
+  }
+  
+  function sort() {
+    document
+      .querySelector(".filter-wrapper")
+      .classList.toggle("filter-wrapper-open");
+    document.querySelector("body").classList.toggle("filter-wrapper-overlay");
+  }
+
 inputElement.addEventListener("input", () => {
   handleInputChange(inputElement);
 });
-search_icon.addEventListener("click", handleSearchCloseOnClick);
-sort_wrapper.addEventListener("click", handleSortIconOnClick);
+search_icon.addEventListener("click", search);
+sort_wrapper.addEventListener("click", sort);
 
 function handleInputChange(inputElement) {
   const inputValue = inputElement.value;
@@ -20,18 +34,4 @@ function handleInputChange(inputElement) {
       .querySelector("#search-close-icon")
       .classList.remove("search-close-icon-visible");
   }
-}
-
-function handleSearchCloseOnClick() {
-  document.querySelector("#search-input").value = "";
-  document
-    .querySelector("#search-close-icon")
-    .classList.remove("search-close-icon-visible");
-}
-
-function handleSortIconOnClick() {
-  document
-    .querySelector(".filter-wrapper")
-    .classList.toggle("filter-wrapper-open");
-  document.querySelector("body").classList.toggle("filter-wrapper-overlay");
 }
