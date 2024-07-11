bbdfe2c205471724b2dea10e55b694ee44f30479
Shegofa <shogofadeveloper12@gmail.com>
2024-07-11 17:33:03 +0400

    linter errors of script.js

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const MAX_POKEMON = 151;
-const listWrapper = document.querySelector(".list-wrapper");
-const searchInput = document.querySelector("#search-input");
-const numberFilter = document.querySelector("#number");
-const nameFilter = document.querySelector("#name");
-const notFoundMessage = document.querySelector("#not-found-message");
+const listWrapper = document.querySelector('.list-wrapper');
+const searchInput = document.querySelector('#search-input');
+const numberFilter = document.querySelector('#number');
+const nameFilter = document.querySelector('#name');
+const notFoundMessage = document.querySelector('#not-found-message');
 
 let allPokemons = [];
 
+async function fetchPokemonDataBeforeRedirect(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json()
+      ),
+    ]);
+    return { pokemon, pokemonSpecies };
+  } catch (error) {
+    console.error('Failed to fetch Pokemon data before redirect', error);
+    return null;
+  }
+}
+
 function displayPokemons(pokemon) {
-  listWrapper.innerHTML = "";
+  listWrapper.innerHTML = '';
 
   pokemon.forEach((pokemon) => {
-    const pokemonID = pokemon.url.split("/")[6];
-    const listItem = document.createElement("div");
-    listItem.className = "list-item";
+    const pokemonID = pokemon.url.split('/')[6];
+    const listItem = document.createElement('div');
+    listItem.className = 'list-item';
     listItem.innerHTML = `
         <div class='number-wrap'>
             <p class='caption-fonts'>#${pokemonID}</p>
@@ -26,7 +43,7 @@ function displayPokemons(pokemon) {
         </div>
     `;
 
-    listItem.addEventListener("click", async () => {
+    listItem.addEventListener('click', async () => {
       const success = await fetchPokemonDataBeforeRedirect(pokemonID);
       if (success) {
         window.location.href = `./detail.html?id=${pokemonID}`;
@@ -44,34 +61,17 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
     displayPokemons(allPokemons);
   });
 
-async function fetchPokemonDataBeforeRedirect(id) {
-  try {
-    const [pokemon, pokemonSpecies] = await Promise.all([
-      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
-        res.json()
-      ),
-      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
-        res.json()
-      ),
-    ]);
-    return true;
-  } catch (error) {
-    console.error("Failed to fetch Pokemon data before redirect");
-  }
-}
-
 function handleSearch() {
   const searchTerm = searchInput.value.toLowerCase();
   let filteredPokemons;
 
   if (numberFilter.checked) {
     filteredPokemons = allPokemons.filter((pokemon) => {
-      const pokemonID = pokemon.url.split("/")[6];
+      const pokemonID = pokemon.url.split('/')[6];
       return pokemonID.startsWith(searchTerm);
     });
   } else if (nameFilter.checked) {
-    filteredPokemons = allPokemons.filter((pokemon) =>
-      pokemon.name.toLowerCase().startsWith(searchTerm)
+    filteredPokemons = allPokemons.filter((pokemon) => pokemon.name.toLowerCase().startsWith(searchTerm)
     );
   } else {
     filteredPokemons = allPokemons;
@@ -80,19 +80,19 @@ function handleSearch() {
   displayPokemons(filteredPokemons);
 
   if (filteredPokemons.length === 0) {
-    notFoundMessage.style.display = "block";
+    notFoundMessage.style.display = 'block';
   } else {
-    notFoundMessage.style.display = "none";
+    notFoundMessage.style.display = 'none';
   }
 }
 
-searchInput.addEventListener("keyup", handleSearch);
+searchInput.addEventListener('keyup', handleSearch);
 
 function clearSearch() {
-  searchInput.value = "";
+  searchInput.value = '';
   displayPokemons(allPokemons);
-  notFoundMessage.style.display = "none";
+  notFoundMessage.style.display = 'none';
 }
 
-const closeButton = document.querySelector(".search-close-icon");
-closeButton.addEventListener("click", clearSearch);
+const closeButton = document.querySelector('.search-close-icon');
+closeButton.addEventListener('click', clearSearch);
